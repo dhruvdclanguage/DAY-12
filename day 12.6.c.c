24fcec1bb503b4212c9a0f4df84354468c89a801
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-03-03 13:15:53 +0530

    Add files via upload

---
+++ day 12.6.c.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int number=1;
+	int n;
+	
+     	printf("Enter the value of N: ");
+     	scanf("%d",&n);
+
+
+	    printf("Even Numbers from 1 to %d:\n",n);
+ 
+    while(number<=n)	{
+	
+		if(number%2==0)
+			printf("%d ",number);
+			number++;
+	}
+
+	return 0;
+}

4e779191e7e94aa1779c34e378f9ab090c9ce401
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-03-03 12:45:59 +0530

    Add files via upload

---
+++ day 12.3.c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main () {
+
+	
+	int num , a = 1;
+	
+	     printf("Enter psitive number:");
+	     scanf("%d",&a);
+	     
+	while (a<=num) {
+		  
+		  printf("%d\n",a);
+		  a++;
+}
+       return 0;
+       
+}
+
---
+++ day 12.4.c.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main () {
+
+	
+	int i=1,n;
+	
+	     printf("Enter psitive number :");
+	     scanf("%d",&n);
+	     
+	while (i>=1) {
+		  
+		  printf("%d\n",i);
+		  i--;
+}
+       return 0;
+       
+}
